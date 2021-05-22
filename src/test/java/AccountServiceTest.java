6ce47f945f2ddaf6c930838fc149d2f4dea45634
Phil <pfestoso@redhat.com>
2021-05-22 15:46:56 -0500

    add test component and class

--- src/test/java/AccountServiceTest.java
+++ src/test/java/AccountServiceTest.java
@@ -1,11 +1,17 @@
+import org.junit.Before;
 import org.junit.Rule;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.springframework.test.context.TestPropertySource;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
-
+@TestPropertySource(properties = {"service.wsdl.url=localhost:8080?url"})
 public class AccountServiceTest {
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(options().port(8088),false);
+
+
+    private String wsdlUrl = "";
+    private String endpointUrl = "";
 }
